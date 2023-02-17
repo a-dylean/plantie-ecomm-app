61264812eca52f08b1a9b3fe12fc038c9cb3691f
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-02-17 16:17:51 +0100

    Implementing products REST API

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,7 +3,23 @@ const pool = require("../database");
 
 productsRouter = express.Router();
 
-productsRouter.get('/products', async (req, res, next) => {
+//create a product
+productsRouter.post('/', async (req, res) => {
+    try {
+        const {name, description, price, available, category_id} = req.body;
+        const newProduct = await pool.query(
+            "INSERT INTO products (name, description, price, available) VALUES ($1, $2, $3, $4, $5) RETURNING *", [name, description, price, available, category_id]
+        );
+
+        res.json(newProduct.rows[0])
+    } catch (err) {
+        console.error(err.message);
+    }
+    console.log(req.body)
+});
+
+//get all products
+productsRouter.get('/', async (req, res, next) => {
     try {
         const allProducts = await pool.query("SELECT * FROM products");
         res.json(allProducts.rows);
