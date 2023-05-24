8261b0149735600aa8c2725627cca506faeca02b
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-05-24 11:58:25 +0200

    Change in useRetrieveSession hook to fix the bug on logout

--- client/src/hooks/useRetrieveSession.ts
+++ client/src/hooks/useRetrieveSession.ts
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useCreateNewUserMutation } from '../features/users/usersApi';
+import { useGetCurrentUserDetailsQuery } from '../features/users/usersApi';
 
 export const useRetrieveSession = () => {
-    const [userId, setUserId] = useState(0);
-    const token = localStorage.getItem("accessToken");
-    const [startSession] = useCreateNewUserMutation();
-    const createNewUser = async () => {
-      const result = await startSession().unwrap();
-      setUserId(result.id);
-      localStorage.setItem("accessToken", result.accessToken);
-    };
-    useEffect(() => {
-      if (!token) {
-        createNewUser();
-      }
-    }, [userId]); 
-}
+  const { data: user } = useGetCurrentUserDetailsQuery();
+  const [startSession] = useCreateNewUserMutation();
+
+  const createNewUser = async () => {
+    const result = await startSession().unwrap();
+    localStorage.setItem('accessToken', result.accessToken);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (!token) {
+      createNewUser();
+    }
+  }, [user]);
+};
