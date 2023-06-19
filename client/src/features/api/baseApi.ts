c635daaa52c1583db7b743109093378b12c43ca3
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-06-19 18:34:56 +0200

    Change env file

--- client/src/features/api/baseApi.ts
+++ client/src/features/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { securelyGetAccessToken } from '../../helpers/refreshToken';
 import { BASE_URL } from '../../appconfig';
 
-const baseUrl = BASE_URL || "http://localhost:4001";
+const baseUrl = BASE_URL;
 
 const baseQuery = fetchBaseQuery({
   baseUrl,
