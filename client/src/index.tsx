6b89841b74c6a9ee2c30da0617be2b904edfaab9
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-10-19 18:37:15 +0200

    Changes to config files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './app/app';
-import { StrictMode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 export const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  // <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  // </StrictMode>,
 );
